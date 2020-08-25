04499192a5e0d8d4ee6a51a08cdbd214e77e0a3e
Maheshborra619 <maheshtroy@gmail.com>
2020-08-25 13:11:34 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React,{useState} from "react";
+import {Container} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+
+
+import Todos from "./Components/Todos";
+import TodoForm from "./Components/TodoForm";
+import { useEffect } from "react";
+
+const App = () => {
+    const [todos, setTodos] = useState([])
+
+
+    useEffect(()=>{
+        const localTodos = localStorage.getItem("todos");
+        console.log({ localStorage });
+       if(localTodos){
+           setTodos(JSON.parse(localTodos));
+       }
+    }, []);
+
+    const addTodos = async todo =>{
+        setTodos([...todos,todo])
+    }
+
+    useEffect(()=>{
+      localStorage.setItem("todos",JSON.stringify(todos));
+    },[todos]);
+
+    const markComplete = id =>{
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
+    return(
+        <Container fluid>
+            <h1>Todo with React @Mahesh Borra</h1>
+            <TodoForm addTodos={addTodos}/>
+            <Todos todos={todos} markComplete={markComplete} />
+        </Container>
+    )
+}
+export default App;
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,60 @@
+import React,{useState} from 'react';
+
+import {
+    FormGroup,
+    InputGroup,
+    Input,
+    InputGroupAddon,
+    Button,
+    Form
+} from "reactstrap";
+
+import {v4} from "uuid";
+
+
+const TodoForm = ({addTodos}) =>{
+
+    const [todoString, setTodoString] = useState("");
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        if(todoString === ""){
+            return alert("Please enter someting");
+        }
+
+        const todo = {
+            todoString,
+            id:v4() 
+        }
+
+        //TODO - method props 
+        addTodos(todo);
+   
+        setTodoString("");
+    }
+  return (
+      <Form onSubmit={handleSubmit}>
+          <FormGroup>
+              <InputGroup>
+              <Input
+              type="text"
+              name="todo"
+              id="todo"
+              placeholder="Enter a todo please"
+              value={todoString}
+              onChange= {e => setTodoString(e.target.value)} />
+              <InputGroupAddon addonType="prepend">
+                  <Button
+                  color="success">
+                     Add todo</Button>
+              
+              </InputGroupAddon>
+              </InputGroup>
+          </FormGroup>
+      </Form>
+  )
+}
+
+export default TodoForm;
+
